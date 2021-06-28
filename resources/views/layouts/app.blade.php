424946e9f967ad47f12be9302706608986996605
JeZoS <prabhat262111@gmail.com>
2021-06-28 16:05:55 +0530

    added login and register

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -5,8 +5,39 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Posty</title>
+    <link rel="stylesheet" href="{{asset('css/app.css')}}">
 </head>
-<body>
+<body class="bg-gray-200" >
+    <nav class="p-6 bg-white mb-3 flex justify-between">
+        <ul class="flex items-center">
+            <li>
+                <a href="" class="p-3">Home</a>
+            </li>
+            <li>
+                <a href="" class="p-3">Dashboard</a>
+            </li>
+            <li>
+                <a href="" class="p-3">Post</a>
+            </li>
+        </ul>
+        <ul class="flex items-center">
+            @if (Auth::user())
+                <li>
+                    <a href="" class="p-3">Name</a>
+                </li>                
+                <li>
+                    <a href="" class="p-3">Logout</a>
+                </li>
+            @else
+                <li>
+                    <a href="{{route('login')}}" class="p-3">Login</a>
+                </li>
+                <li>
+                    <a href="{{route('register')}}" class="p-3">Register</a>
+                </li>                
+            @endif
+        </ul>
+    </nav>
     @yield('content')
 </body>
 </html>
